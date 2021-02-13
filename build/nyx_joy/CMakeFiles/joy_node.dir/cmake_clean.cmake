ebc79d7c0dcfefa2a59f4b12725be6c4a3f7cdc7
Raisintoe <derek.workman@aggiemail.usu.edu>
2021-02-13 15:54:54 -0700

    Initial commit - some old code from the Raspi labeled nyx_ws

---
+++ build/nyx_joy/CMakeFiles/joy_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/joy_node.dir/src/joy_node.cpp.o"
+  "/home/ares/nyx_ws/devel/lib/nyx_joy/joy_node.pdb"
+  "/home/ares/nyx_ws/devel/lib/nyx_joy/joy_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/joy_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/nyx_joy/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ares/nyx_ws/src/nyx_joy
+# Build directory: /home/ares/nyx_ws/build/nyx_joy
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
